3c45d247d8a70e6c6d106a1b582aa1874a22f67b
stripodi <simone.tripodi@terradue.com>
2025-08-12 12:14:00 +0200

    feat: dependency updated to "cwl-loader==0.1.0", removed now useless cwl-loader installation from git, added missing test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,9 @@
-# SPDX-FileCopyrightText: 2025-present Fabrice Brito <fabrice.brito@terradue.com>
-#
-# SPDX-License-Identifier: MIT
+"""
+CWL2PlantUML (c) 2025
+
+CWL2PlantUML is licensed under
+Creative Commons Attribution-ShareAlike 4.0 International.
+
+You should have received a copy of the license along with this work.
+If not, see <https://creativecommons.org/licenses/by-sa/4.0/>.
+"""
