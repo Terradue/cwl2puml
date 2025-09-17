22ead76a7516ce8e082a045548afd8115aec0c6e
stripodi <simone.tripodi@terradue.com>
2025-09-17 18:11:50 +0200

    feat: CLI and API separation of concerns

--- src/cwl2puml/__init__.py
+++ src/cwl2puml/__init__.py
@@ -11,32 +11,23 @@ You should have received a copy of the license along with this work.
 If not, see <https://creativecommons.org/licenses/by-sa/4.0/>.
 """
 
-from cwl_loader import (
-    load_cwl_from_location,
-    Processes,
-    Stream
-)
 from cwl_utils.parser import Process
-from datetime import datetime
 from enum import (
     auto,
     Enum
 )
-from io import TextIOBase
 from jinja2 import (
     Environment,
     PackageLoader
 )
-from loguru import logger
-from pathlib import Path
 from typing import (
     Any,
+    List,
     Union,
+    TextIO,
     get_args,
     get_origin
 )
-import click
-import time
 
 class DiagramType(Enum):
     '''The supported PlantUML diagram types'''
@@ -65,18 +56,18 @@ def _type_to_string(typ: Any) -> str:
 
     return typ.__name__
 
-env = Environment(
+_jinja_environment = Environment(
     loader=PackageLoader(
         package_name='cwl2puml'
     )
 )
-env.filters['to_puml_name'] = _to_puml_name
-env.filters['type_to_string'] = _type_to_string
+_jinja_environment.filters['to_puml_name'] = _to_puml_name
+_jinja_environment.filters['type_to_string'] = _type_to_string
 
 def to_puml(
-    cwl_document: Processes,
+    cwl_document: Process | List[Process],
     diagram_type: DiagramType,
-    output_stream: Stream
+    output_stream: TextIO
 ):
     '''
     Converts a CWL,m given its document model, to a PlantUML diagram.
@@ -89,52 +80,8 @@ def to_puml(
     Returns:
         `None`: none
     '''
-    template = env.get_template(f"{diagram_type.name.lower()}.puml")
+    template = _jinja_environment.get_template(f"{diagram_type.name.lower()}.puml")
 
     workflows = cwl_document if isinstance(cwl_document, list) else [cwl_document]
 
     output_stream.write(template.render(workflows=workflows))
-
-@click.command()
-@click.option("--workflow", required=True, help="The CWL workflow file (it can be an URL or a file on the File System)")
-@click.option('--puml', type=click.Choice(DiagramType, case_sensitive=False), required=True, help="The PlantUML diagram type.")
-@click.option("--output", type=click.Path(path_type=Path), required=True, help="Output file path")
-def main(
-    workflow: str,
-    puml: DiagramType,
-    output: Path
-):
-    '''
-    Converts a CWL,m given its document model, to a PlantUML diagram.
-
-    Args:
-        `workflow` (`str`): The CWL workflow file (it can be an URL or a file on the File System)
-        `puml` (`DiagramType`): The PlantUML diagram type to render
-        `output` (`Path`): The output file where streaming the PlantUML diagram
-
-    Returns:
-        `None`: none
-    '''
-    start_time = time.time()
-
-    cwl_document = load_cwl_from_location(path=workflow)
-
-    logger.info('------------------------------------------------------------------------')
-
-    output.parent.mkdir(parents=True, exist_ok=True)
-
-    logger.info(f"Saving the new PlantUML Workflow diagram to {output}...")
-
-    with output.open("w") as f:
-        to_puml(
-            cwl_document=cwl_document,
-            diagram_type=puml,
-            output_stream=f
-        )
-
-    logger.info(f"PlantUML Workflow {puml.name.lower()} diagram successfully rendered to {output}!")
-
-    end_time = time.time()
-
-    logger.info(f"Total time: {end_time - start_time:.4f} seconds")
-    logger.info(f"Finished at: {datetime.fromtimestamp(end_time).isoformat(timespec='milliseconds')}")
